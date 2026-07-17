b5be03889b2fdfa668a34f4e28d13506ca781ef6
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-17 23:40:40 +0300

    feat: update Job schema and resolver to include job date attribute

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,4 +4,8 @@ export const resolvers = {
   Query: {
     jobs: () => getJobs(),
   },
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
+  },
 };
+const toIsoDate = (value) => value.split('T')[0];
