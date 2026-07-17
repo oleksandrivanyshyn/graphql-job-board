060c55b4be5861f3b3560913386545eb04cc810e
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-07-17 23:04:22 +0300

    chore: add GraphQL server setup with schema and resolvers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import cors from 'cors';
 import express from 'express';
 import { authMiddleware, handleLogin } from './auth.js';
+import { ApolloServer } from '@apollo/server';
+import { readFile } from 'fs/promises';
+import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
 
 const PORT = 9000;
 
@@ -9,6 +12,14 @@ app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
 
+const typeDefs = await readFile('./schema.graphql', 'utf-8');
+const { resolvers } = await import('./resolvers.js');
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+app.use('/graphql', apolloMiddleware(apolloServer));
+
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
 });
